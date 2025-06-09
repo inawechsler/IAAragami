d3651c4aed556425c45cd73e834a8ca3afaac60e
inawechsler <inaki.wechsler@gmail.com>
2025-06-09 20:47:36 -0300

    Final 2.0

--- Assets/Scripts/IA/Enemy/FlyEnemy/States/FAISRunAway.cs
+++ Assets/Scripts/IA/Enemy/FlyEnemy/States/FAISRunAway.cs
@@ -5,12 +5,14 @@ public class FAISRunAway<T> : AISBase<T>
     private Vector3 _target;
     private ISteering _steering;
     private PathfindingBehaviour _pathfindingBehaviour;
+    private LeaderBehaviour _leaderBehaviour;
+
     public FAISRunAway(ISteering steering, Vector3 target, LeaderBehaviour leader, PathfindingBehaviour pathfindingBehaviour)
     {
         _steering = steering;
         _target = target;
         _pathfindingBehaviour = pathfindingBehaviour;
-        leader.IsActive = false;
+        _leaderBehaviour = leader;
     }
 
     public override void Enter()
@@ -18,8 +20,9 @@ public class FAISRunAway<T> : AISBase<T>
         base.Enter();
 
         _pathfindingBehaviour.IsActive = true;
-
         _pathfindingBehaviour.GeneratePathTo(_target);
+
+        _leaderBehaviour.IsActive = false;
 
         if (path != null)
         {
@@ -33,6 +36,7 @@ public class FAISRunAway<T> : AISBase<T>
         base.Execute();
 
         if (path.isFinishPath) return;
+
         if (_pathfindingBehaviour.IsPathCompleted)
         {
             if (path != null)
@@ -40,16 +44,13 @@ public class FAISRunAway<T> : AISBase<T>
                 path.isFinishPath = true;
                 path.isOnPathfinding = false;
             }
-            Debug.Log("Pathfinding con flocking completado");
             return;
         }
 
-        
+        var flockingDir = _steering.GetDir();
+        flockingDir.y = 0;
 
-        var steeringDir = _steering.GetDir();
-        steeringDir.y = 0;
-
-        var finalDir = obs.GetDir(steeringDir);
+        var finalDir = obs.GetDir(flockingDir);
 
         if (finalDir.magnitude > 0.1f)
         {
@@ -63,15 +64,7 @@ public class FAISRunAway<T> : AISBase<T>
         base.Exit();
 
         _pathfindingBehaviour.IsActive = false;
-    }
 
-    public void SetNewTarget(Vector3 newTarget)
-    {
-        _target = newTarget;
-        if (_pathfindingBehaviour.IsActive)
-        {
-            _pathfindingBehaviour.GeneratePathTo(_target);
-        }
+        _leaderBehaviour.IsActive = false;
     }
 }
-
